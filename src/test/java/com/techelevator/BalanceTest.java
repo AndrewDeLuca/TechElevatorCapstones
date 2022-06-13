a4779a8c2c781b7f87b25bf67b2d93c1333d22cf
Bradley Mauger <bradleyamauger@gmail.com>
2022-06-13 16:24:38 -0400

    sdl;kfj

--- src/test/java/com/techelevator/BalanceTest.java
+++ src/test/java/com/techelevator/BalanceTest.java
@@ -1,8 +1,12 @@
 package com.techelevator;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BalanceTest {
+    @Test
+    public void addToCurrentMoney() {
 
+    }
 }
